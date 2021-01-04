8ea7412b7226bddf8bff47f2de03e49482e0420e
shatha9522 <shatha_abuali95@hotmail.com>
2021-01-04 16:24:55 +0200

    firsttimetopush

---
+++ js/app.js
@@ -0,0 +1,59 @@
+var userName =prompt('What is your name?');
+ alert(' Hello ' + userName + ' Welcome to my page ');
+ alert( 'Please Answer these Qustions to see how much do you know me');
+ var favmovie = prompt('What Is My Favourite Movie ?');
+console.log('Favourite Movie is', favmovie);
+var myfavmovie ='shawshak redumbtion';
+if(favmovie.toLowerCase() === myfavmovie){
+    alert('correct! shawshak redumbtion is my favorite movie!');
+  } else {
+    alert('unfortunately '+ favmovie + ' is not my favorite' );
+  }
+  
+  var favdish = prompt('What Is My Favourite Dish ?');
+  console.log('Favourite Dish is', favmovie);
+  var myfavdish ='pasta';
+  if(favdish.toLowerCase() === myfavdish){
+      alert('correct! pasta is my favorite Dish!');
+    } else {
+      alert('unfortunately '+ favdish + ' is not my favorite' );
+    }
+
+    var favcolor = prompt('What Is My Favourite color ?');
+  console.log('Favourite color is', favcolor);
+  var myfavcolor ='purple';
+  if(favcolor.toLowerCase() === myfavcolor){
+      alert('correct! purple is my favorite Color!');
+    } else {
+      alert('unfortunately '+ favcolor + ' is not my favorite' );
+    }
+    var favtime = prompt('Day or Night?');
+    console.log('Favourite time is', favtime);
+    var myfavtime ='day';
+    if(favtime.toLowerCase() === myfavtime){
+        alert('correct! Im a Day person');
+      } else {
+        alert('oops! Im not a Night person' );
+      }
+      var favflower = prompt('What Is My favorite flower ?');
+  console.log('Favourite flower is', favflower);
+  var myfavflower ='jasmin';
+  if(favflower.toLowerCase() === myfavflower){
+      alert('Yup! I love jasmin');
+    } else {
+      alert(' I dont like '+ favflower  );
+    }
+
+
+//var favfootballteam = prompt('What Is Your Favourite Football Team ?');
+//console.log('My Favourite football team is ', favfootballteam);
+
+//var socialmediatime = prompt('How Much Time Do You spend on Social Media ?');
+//console.log('Social Media Time', socialmediatime + 'hours');
+
+//var favplace = prompt('Sea or Mountain ?');
+//console.log('My favourite place is', favplace );
+
+ 
+
+
