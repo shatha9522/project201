d33cff3ae7e40e2f5341777f07ff77e613439d2d
shatha9522 <shatha_abuali95@hotmail.com>
2021-01-05 22:53:25 +0200

    push

--- js/app.js
+++ js/app.js
@@ -1,55 +1,83 @@
-var userName =prompt('What is your name?');
- alert(' Hello ' + userName + ' Welcome to my page ');
- alert( 'Please Answer these Qustions to see how much do you know me');
- var favmovie = prompt('shawshak redumbtion Is My Favourite Movie ?');
+var userName = prompt('What is your name?');
+alert(' Hello ' + userName + ' Welcome to my page ');
+alert('Please Answer these Qustions to see how much do you know me');
+var favmovie = prompt('shawshak redumbtion Is My Favourite Movie ?');
 console.log(favmovie);
-if(favmovie.toLowerCase() === 'yes' || favmovie === 'y'){
-    alert('correct! shawshak redumbtion is my favorite movie!');
-  } else {
-    alert('unfortunately your answer is not correct' );
+if (favmovie.toLowerCase() === 'yes' || favmovie === 'y') {
+  alert('correct! shawshak redumbtion is my favorite movie!');
+} else {
+  alert('unfortunately your answer is not correct');
+}
+
+var favdish = prompt('Do I like Dounate ?');
+console.log(favdish);
+if (favdish.toLowerCase() === 'yes' || favdish === 'y') {
+  alert('correct!');
+} else {
+  alert('unfortunately, I like Dounate');
+}
+
+var favcolor = prompt('Do I Like purple ?');
+console.log(favcolor);
+if (favcolor.toLowerCase() === 'yes' || favcolor === 'y') {
+  alert('correct! purple is my favorite Color!');
+} else {
+  alert('Your answer is not correct!');
+}
+var favtime = prompt('Do I like Day time');
+console.log(favtime);
+if (favtime.toLowerCase() === 'yes' || favtime === 'y') {
+  alert('correct! Im a Day person');
+} else {
+  alert('oops! Im not a Night person');
+}
+var favflower = prompt('Do I like Jasmin ?');
+console.log(favflower);
+var myfavflower = 'jasmin';
+if (favflower.toLowerCase() === 'yes' || favflower === 'y') {
+  alert('Yup! I love jasmin');
+} else {
+  alert('Your answer is not correct!');
+}
+
+for (var i = 0; i < 4; i++) {
+  var num1 = prompt('How old am I?');
+  console.log(num1);
+  var age = 25;
+  if (num1 === age) {
+    alert('correct');
   }
-  
-  var favdish = prompt('Do I like Dounate ?');
-  console.log(favdish);
-  if(favdish.toLowerCase() === 'yes' || favdish === 'y' ){
-      alert('correct!');
-    } else {
-      alert('unfortunately, I like Dounate' );
-    }
+  else if (num1 > age) {
+    alert('too High');
+  }
+  else {
+    alert('too low');
+  }
+}
+alert('correct answer is 25');
 
-    var favcolor = prompt('Do I Like purple ?');
-  console.log( favcolor);
-  if(favcolor.toLowerCase() === 'yes' || favcolor === 'y'){
-      alert('correct! purple is my favorite Color!');
-    } else {
-      alert('Your answer is not correct!' );
-    }
-    var favtime = prompt('Do I like Day time');
-    console.log( favtime);
-    if(favtime.toLowerCase() === 'yes' || favtime === 'y'){
-        alert('correct! Im a Day person');
-      } else {
-        alert('oops! Im not a Night person' );
-      }
-      var favflower = prompt('Do I like Jasmin ?');
-  console.log( favflower);
-  var myfavflower ='jasmin';
-  if(favflower.toLowerCase() === 'yes' || favflower === 'y'){
-      alert('Yup! I love jasmin');
-    } else {
-      alert( 'Your answer is not correct!' );
-    }
+for (var i = 0; i < 4; i++) {
+  var num2 = prompt('gusse my hight');
+  console.log(num2)
+  var hight = 160;
+  if (num2 === hight) {
+    alert('correct');
+  }
+  else if (num2 > hight) {
+    alert('too High');
+  }
+  else {
+    alert('too low')
+  }
+}
+alert('correct answer is 160');
 
 
-//var favfootballteam = prompt('What Is Your Favourite Football Team ?');
-//console.log('My Favourite football team is ', favfootballteam);
 
-//var socialmediatime = prompt('How Much Time Do You spend on Social Media ?');
-//console.log('Social Media Time', socialmediatime + 'hours');
 
-//var favplace = prompt('Sea or Mountain ?');
-//console.log('My favourite place is', favplace );
 
- 
+
+
+
 
 
